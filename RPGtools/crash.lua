b78ffdb0ee5e9699a0d047db4d9342536093d254
callum@callum-dawson.net <callum@callum-dawson.net@54540c62-9bdf-b5b4-085c-f64c53ed4ef5>
2010-04-21 18:14:00 +0000

    Added a crash script.

---
+++ RPGtools/crash.lua
@@ -0,0 +1,6 @@
+function crash(loss)
+	if (getVehicleController(source)) then
+		setElementHealth(getVehicleController(source),getElementHealth(getVehicleController(source))-math.floor(loss)/10)
+	end
+end
+addEventHandler("onVehicleDamage",getRootElement(),loss)
