e2b5aeced325dde52ff9bb23c4a79d69a3033cde
callum <callum@callum-dawson.net>
2010-05-07 22:36:37 +0000

    Deleted old RPG gamemode.

--- RPGanticheat/server.lua
+++
@@ -1,6 +0,0 @@
-function vehCheck()
-	if (math.floor(getElementHealth(source)) > 1000) then
-		kickPlayer(getVehicleController(source),"Anti-Cheat: Vehicle Health Cheat")
-	end
-end
-addEventHandler("onVehicleDamage",getRootElement(),vehCheck)
--- RPGtools/weapons.client.lua
+++
@@ -1,8 +0,0 @@
-function medicFlower(attacker,weapon,bodypart,loss)
-	if (weapon == 14) then
-		if (getElementData(attacker,"class") == "Paramedic") then
-			setElementHealth(source,getElementHealth(source)+loss)
-		end
-	end
-end
-addEventHandler("onClientPlayerDamage",getLocalPlayer(),medicFlower)
